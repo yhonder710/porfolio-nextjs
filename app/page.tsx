8dc211b345b64537cce466a6c9bff48965582950
kicknash01 <olivercordero0000@gmail.com>
2025-03-13 16:33:00 -0400

    colores de iconos de inicio

--- app/page.tsx
+++ app/page.tsx
@@ -14,17 +14,25 @@ export default function page () {
       <section className="flex">
         <div className="w-[60%] h-80 flex flex-col gap-5 items-center justify-center">
           <h2 className="text-4xl font-bold text-center ">Desarrolador Full Stack Autodidacta</h2>
-          <div className="flex border-2 rounded-3xl w-120 text-5xl justify-center items-center gap-8 py-3.5 ">
-          <FaFacebook />
-          <BsInstagram />
-          <IoLogoTiktok />
-          <SiWhatsapp />
-          <VscGithubInverted />
+            <div className="flex border-2 rounded-xl w-120 text-5xl justify-center items-center gap-8 py-3.5 ">
+              <FaFacebook  className="bg-white text-[#2c7ffa] rounded-4xl"/>
+              <BsInstagram className="bg-gradient-to-r from-pink-500 via-yellow-400 to-orange-500 p-1 rounded-[14px] text-white "/>
+              <IoLogoTiktok className="bg-black rounded-3xl p-1"/>
+              <SiWhatsapp className="bg-green-600 rounded-3xl p-[5px]"/>
+              <VscGithubInverted className="bg-black rounded-3xl p-1"/>
+            </div>
+          <div>
+          <ul className="text-[21px] text-left w-155">
+            <li>WhatsApp: Envíanos un mensaje directo para consultas rápidas.</li>
+            <li>Instagram: Síguenos y envíanos un DM para interactuar.</li>
+            <li>TikTok: Conéctate con nosotros y déjanos un comentario.</li>
+            <li>Facebook: Visítanos y envíanos un mensaje en nuestra página.</li>
+            <li>GitHub: Si tienes preguntas sobre proyectos, no dudes en abrir un issue.</li>
+          </ul>
           </div>
-          <p className="text-2xl text-left w-155">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum officia pariatur delectus nam, maiores quia cumque ducimus nobis iste saepe ipsum numquam facilis in porro non temporibus aliquam omnis necessitatibus?</p>
         </div>
         <div className="w-[40%] h-80 flex justify-center items-center">
-          <img className="rounded-[100%] w-80 h-80 object-cover" src="https://wiki.leagueoflegends.com/en-us/images/Chibi_Akali_Base_Tier_1.png?6ebb1"/>
+          <img className="rounded-[100%] w-80 h-80 object-cover" src="https://www.shutterstock.com/image-vector/programmer-computer-expert-black-linear-600nw-2033137370.jpg"/>
         </div>
       </section>
     </main>
